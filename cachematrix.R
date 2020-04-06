217e2af6b739514da289b580efadf98d1f2ad512
Markos Hay <markoshay@MacBook-Pro-2.fritz.box>
2020-04-06 14:05:17 +0100

    Commited from RStudio

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Solving for (B)
 
-## Write a short comment describing this function
+B <- matrix(c(1,2,3,4),2,2)
 
-makeCacheMatrix <- function(x = matrix()) {
+#Inverse returned (computation)
 
-}
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1)
 
+#Inverse returned (cache)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve(B1)
